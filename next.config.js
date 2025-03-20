fc3af4e5007ff4500ad38d6d48ed73617d0bd733
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2025-03-20 11:07:26 +0530

    Error Resolved, Install previos version of packages and reconfigure files

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "referrer-policy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
