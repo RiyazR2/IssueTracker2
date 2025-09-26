1a1c08c246ea861bee68fe956225fb11523f4127
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2025-09-26 21:35:56 +0530

    updated

--- prisma/migrations/20250926150000_init/migration.sql
+++ prisma/migrations/20250926150000_init/migration.sql
@@ -80,3 +80,4 @@ ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user
 
 -- AddForeignKey
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
